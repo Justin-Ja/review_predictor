a324c128622a470847e5b212b5ba1651ba5f87a9
Justin <yustinyakel100@gmail.com>
2024-08-01 15:50:06 -0400

    More frontend testing and developing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css'; 
+import Header from './components/Header';
+// The app is gonna be super ugly rn. Thats gonna stay until api is up
+// or when it actually works :|
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-
+      <header>
+        {/* HEADER -> Should be some form of nav bar/navigation */}
+        
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <a href='https://www.unbc.ca/sites/default/files/sections/web/links.pdf'>Links here</a>
       </header>
-      <p>hehrehrhehreh</p>
+      <Header/>
+      {/* Any level divs should become components. For now keep here and transfer when planning is done. Also look into using fragments <> and 
+      more accessible segments (not divs, section, nav, whatever else) */}
+      <div>
+        <div>
+          <p>
+            Im Text that will be updated! Review here!
+          </p>
+        </div>
+
+        <div>
+          <button>1 star</button>
+          <button>Text 1</button>
+          <button>Text 1</button>
+          <button>Text 1</button>
+          <button>Text 1</button>
+        </div>
+
+        {/* should be same line */}
+        <div>
+          Your Score: 8
+        </div>
+        <div>
+          AI Score: 12434
+        </div>
+      </div>
+      
     </div>
   );
 }
