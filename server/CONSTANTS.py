d8554f8c673b0ab14b571c88be50eb7480eb88c6
Justin <yustinyakel100@gmail.com>
2024-08-16 15:50:15 -0400

    Moved some server constant vars to their own file for ease of updating

---
+++ server/CONSTANTS.py
@@ -0,0 +1,2 @@
+MODEL_NAME = "dummy.pth"
+TEST_FILE_PATH = 'model_files/data/test-00000-of-00001.parquet'
--- server/server.py
+++ server/server.py
@@ -1,21 +1,12 @@
 from flask import Flask, request, render_template, send_from_directory
 from markupsafe import escape
 from get_review_score_and_prediction import get_review_score_pred
-from os import path
-
+import os
+from CONSTANTS import MODEL_NAME, TEST_FILE_PATH
 #flask --app server/server.py run
 #use python3 server.py
 
-#Consts/setup vars are stored here
-#TODO: probably moving this to separate file, CONSTANTS.
-MODEL_NAME = "dummy.pth"
-test_file_path = 'model_files/data/test-00000-of-00001.parquet'
-
 # We create this on setup before anything else
-#TODO: update the split to have all in on one dl. Fix !!! issue in get first then update this
-# dataLoaders_and_vocab = setup_data.create_dataLoaders(test_file_path, 1, 0.25, 0.00020, True)
-# test_dl = dataLoaders_and_vocab[1]
-
 app = Flask(__name__, 
             static_url_path='',
             static_folder='../client/build',
@@ -24,7 +15,7 @@ app = Flask(__name__,
 @app.route("/", defaults={'path': ''})
 @app.route("/<path:path>")
 def serve(path):
-    if path != "" and path.exists(path.join(app.static_folder, path)):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
